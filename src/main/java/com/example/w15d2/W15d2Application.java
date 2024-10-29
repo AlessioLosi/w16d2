05d8410b7e696228d05e019cda4218d938ebfa09
AlessioLosi <alessiolosi93@icloud.com>
2024-10-29 14:58:50 +0100

    Initial commit

---
+++ src/main/java/com/example/w15d2/W15d2Application.java
@@ -0,0 +1,13 @@
+package com.example.w15d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W15d2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W15d2Application.class, args);
+	}
+
+}
